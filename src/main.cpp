f4ebb20a5d0f5c7d8792338bdf3bf45176a253d4
Meindert van Duin <meindertvanduin99@gmail.com>
2021-10-09 14:00:58 +0200

    Create DFT with fftw3

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 #include "pulseaudio/PaSource.h"
-#include <math.h>
-#include <vector>
+#include <fftw3.h>
 
 const uint32_t k_fps = 10;
 const uint32_t sampling_frequency = 44100;
-const uint32_t bars = 40;
-const uint32_t sample_size = 70;
-
-double get_max(std::vector<double> filtered_data);
+const uint32_t sample_size = 10;
 
 int main() {
     auto frames_count = sampling_frequency / k_fps;
+    uint32_t fftw_results = (sampling_frequency / 2) + 1;
 
     PaSource pa_source;
 
@@ -20,38 +17,21 @@ int main() {
             buffer_frame buffer[frames_count];
             pa_source.read(buffer, sizeof(buffer));
 
-            auto block_size = std::floor(frames_count / sample_size);
 
-            std::vector<double> l_values;
-            std::vector<double> r_values;
+            double left[frames_count];
+            for (int j = 0; j < frames_count; j++) {
+                left[j] = buffer[j].l;
+            }
 
-            for (auto j = 0u; j < sample_size; j++) {
-                auto block_start = block_size * j;
-                int64_t sum_l = 0;
-                int64_t sum_r = 0;
-                for (auto k = 0u; k < block_size; k++) {
-                    uint32_t block_index = block_start + k;
-                    sum_r += std::abs(buffer[block_index].r);
-                    sum_l += std::abs(buffer[block_index].l);
-                }
+            fftw_complex* output = static_cast<fftw_complex *>(fftw_malloc(sizeof(fftw_complex) * fftw_results));
 
-                auto r_filtered = sum_r / block_size;
-                auto l_filtered = sum_l / block_size;
+            fftw_plan plan = fftw_plan_dft_r2c_1d(frames_count, left, output, FFTW_ESTIMATE);
+            fftw_execute(plan);
 
-                printf("%f\n", l_filtered);
+            for (auto x : *output) {
+                printf("%f\n", **output);
             }
         }
     }
 }
-
-double get_max(std::vector<double> filtered_data) {
-    double max = 0;
-    for (auto value : filtered_data) {
-        if (value > max)
-            max = value;
-    }
-
-    return max;
-}
-
 
