2fbd608c9e65c774b58054bfcde96569ee86cc0d
Meindert van Duin <meindertvanduin99@gmail.com>
2021-10-11 19:57:41 +0200

    Calculate the widht in cols and rows of the window

--- src/tui/screen.cpp
+++ src/tui/screen.cpp
@@ -1,11 +1,35 @@
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <ctype.h>
+
 #include "screen.h"
+
+Screen::Screen() {
+    m_bars_component = new BarsComponent{{0, 0, 0, 0}};
+    if (!set_window_size()) {
+        // TODO handle error
+    }
+}
 
 Screen::~Screen() {
     delete m_bars_component;
 }
 
 void Screen::render() {
-    m_bars_component->render();
-    // m_terminal_writer.write_to_console();
+    //m_bars_component->render();
+    //m_terminal_writer.write_to_console();
+}
+
+bool Screen::set_window_size() {
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1) {
+        return false;
+    };
+
+    m_window_data.width = ws.ws_col;
+    m_window_data.height = ws.ws_row;
+
+    return true;
 }
 
--- src/tui/screen.h
+++ src/tui/screen.h
@@ -3,14 +3,19 @@
 #include "terminal_writer.h"
 #include "bars_component.h"
 
+struct WindowData {
+    int width, height;
+};
+
 class Screen {
 public:
-    Screen() {
-        m_bars_component = new BarsComponent{{0, 0, 0, 0}};
-    };
+    Screen();
     ~Screen();
     void render();
 private:
     TerminalWriter m_terminal_writer;
     BarsComponent* m_bars_component;
+    WindowData m_window_data;
+
+    bool set_window_size();
 };
